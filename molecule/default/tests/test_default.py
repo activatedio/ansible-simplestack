bd60f5c7566b8963c67d2cbbd5d0f49ad8980fe9
Ben Tomasini <btomasini@activated.io>
2017-07-03 22:05:22 -0700

    Converted vlan to a flat set of variables rather than an array

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -24,7 +24,7 @@ def test_stack_setup_in_kvm_group(host):
     assert 'kvm' in u.groups
 
 def test_vlan_interface(host):
-    f = host.file('/etc/network/interfaces.d/enp0s25.102')
+    f = host.file('/etc/network/interfaces.d/vlan')
     
     assert f.exists
     assert f.user == 'root'
